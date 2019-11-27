1ee5e91e51debc9f7cad92aef93fe576bac7be12
Dayron <dayronyaret@gmail.com>
2019-11-27 19:36:22 +0000

    Comentada la interfaz de addsongs

--- app/src/main/java/es/ulpgc/dayron/spotifly/addSongs/AddSongsContract.java
+++ app/src/main/java/es/ulpgc/dayron/spotifly/addSongs/AddSongsContract.java
@@ -29,12 +29,21 @@ public interface AddSongsContract {
 
     void fetchData();
 
+    /**
+     * Metodo para cerrar sesion
+     */
     void SignOut();
 
     void goLogin();
 
     void goSongs();
 
+    /**
+     * Metodo que coge el contenido de los campos de texto y la uri del archivo y lo guarda en firebase
+     * @param songTitle titulo de la cancion
+     * @param songArtist artista de la cancion
+     * @param path uri del archivo
+     */
     void uploadSong(String songTitle, String songArtist, Uri path);
 
       void goUsers();
@@ -43,9 +52,20 @@ public interface AddSongsContract {
   interface Model {
     String fetchData();
 
+    /**
+     * Metodo que coge el contenido de los campos de texto y la uri del archivo y lo guarda en firebase
+     * @param title titulo de la cancion
+     * @param artist artista de la cancion
+     * @param path uri del archivo
+     * @param callback callback que se llama una vez se termina de subir la cancion a la base de datos
+     */
     void uploadSong(String title, String artist, Uri path, RepositoryContract.UploadSong callback);
 
-    void signOut(RepositoryContract.SignOut signOut);
+    /**
+     * Metodo para cerrar sesion en la aplicacion
+     * @param callback callback que se llama cuando se termina de cerrar la sesion
+     */
+    void signOut(RepositoryContract.SignOut callback);
   }
 
   interface Router {
